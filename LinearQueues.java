f07bfe808f2756eec57ec100a4825dd68815b488
Sugandha <72730060+Sugandha-kadp@users.noreply.github.com>
2022-06-12 22:06:23 +0530

    Linear Queue

---
+++ LinearQueues.java
@@ -0,0 +1,80 @@
+package dsa;
+
+import java.util.Iterator;
+
+public class LinearQueues {
+
+	private int maxSize;
+	private int front;
+	private int rear;
+	private int nItems;
+	private int qArray[];
+	
+	public LinearQueues(int j) {
+		maxSize = j;
+		front = 0;
+		rear = -1;
+		nItems = 0;
+		qArray = new int[maxSize];
+	}
+
+	public void insert(int s) {
+		if(rear == maxSize-1) {
+			System.out.println("Queue is full ! ");
+		}
+		else {
+			qArray[++rear] = s;
+			nItems = nItems+1;
+			//nItems++;
+		}
+	}
+	
+	public int remove() {
+		if(nItems == 0) {
+			System.out.println("Queue is empty");
+			return -99;
+		}
+		else {
+			nItems = nItems - 1;
+			return qArray[front++];
+		}
+		
+	}
+	
+	public int peekFront() {
+		if(nItems == 0) {
+			System.out.println("Queue is empty");
+			return -99;
+		}
+		else {
+			return qArray[front];
+		}
+	}
+	
+	
+	// is Empty method for Queue
+	public boolean isEmpty() {
+		if(nItems == 0) {
+			return true;
+		}
+		else {
+			return false;
+		}	
+	}
+	
+	// is Full method for Queue
+	public boolean isFull() {
+		if(rear == maxSize-1) {
+			return true;
+		}
+		else {
+			return false;
+		}
+			
+	}
+	
+	public static void main(String[] args) {
+		
+	}
+
+}
